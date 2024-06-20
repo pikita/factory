41b593cb3217c52b39684a5e0bb49be61bc7f25c
massi <cadeddu.massi88@gmail.com>
2024-06-20 14:03:46 +0200

    cambiamenti importanti al layout di base, implementazione video e foto, prosimo passo main page

---
+++ text-animation.js
@@ -0,0 +1,18 @@
+import SplitType from'split-type'
+
+
+let typeSplit = new SplitType('[animate]', {
+  types: 'lines, words, chars',
+  tagName: 'span'
+})
+
+gsap.from('[animate] .line', {
+  y: '100%',
+  opacity: 0,
+  duration: 1,
+  ease: 'circ.out',
+  stagger: 0.25,
+})
+
+
+
